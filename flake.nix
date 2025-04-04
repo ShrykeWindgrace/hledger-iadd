21115e5c700650e33cb9492cd33f5ed5d2541ee3
ShrykeWindgrace <shryke.windgrace@gmail.com>
2025-04-04 11:19:21 +0200

    Refactor dev shells and package definition

--- flake.nix
+++ flake.nix
@@ -4,42 +4,34 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    my-utils = {
+      url = "github:ShrykeWindgrace/haskell-flake-utilities";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, my-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
 
         haskellPackages = pkgs.haskellPackages.extend (final: prev: {
-          #hledger = prev.hledger_1_22_2;
           hledger-lib = prev.hledger-lib_1_40;
         });
-        packagePostOverrides = pkg: with pkgs.haskell.lib.compose; pkgs.lib.pipe pkg [
-          disableExecutableProfiling
-          disableLibraryProfiling
-          dontBenchmark
-          dontCoverage
-          dontDistribute
-          dontHaddock
-          dontHyperlinkSource
-          doStrip
-          enableDeadCodeElimination
-          justStaticExecutables
-
-          dontCheck
-        ];
 
         packageName = "hledger-iadd";
       in
       {
-        packages.${packageName} = packagePostOverrides
+        packages.${packageName} = my-utils.packagePostOverrides.${system}
           (haskellPackages.callCabal2nix packageName self rec { });
         packages.default = self.packages.${system}.hledger-iadd;
 
-        devShells.default = (pkgs.mkShell.override { stdenv = pkgs.stdenvNoCC; }) {
-          #buildInputs = [ ];
-          #inputsFrom = map (__getAttr "env") (__attrValues self.packages.${system});
+        devShells.default = my-utils.shellNoCC.${system} { };
+        devShells.custom-shell = my-utils.shellNoCC.${system} {
+          inputsFrom = map (__getAttr "env") (__attrValues self.packages.${system});
         };
         devShell = self.devShells.${system}.default;
       });
